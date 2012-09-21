62e45374fa0fc598ac6bba59d68b1af6e907780b
Teresa Brant <tbrant@commandalkon.com>
2012-09-21 13:52:59 -0400

    adding navigation.html.  adding hbs modules for handlebars

--- web/app.js
+++ web/app.js
@@ -6,8 +6,12 @@
 var express = require('express')
   , routes = require('./routes')
   , http = require('http');
+ // ,fs = require('fs')
+ // , hbs = require('hbs');
 
 var app = express();
+
+//hbs.registerPartial('navigation', fs.readFileSync(__dirname + '/views/navigation.html', 'utf8'));
 
 app.configure(function(){
   app.set('port', process.env.PORT || 4141);
@@ -15,6 +19,7 @@ app.configure(function(){
   //app.set('view engine', 'jade');
   app.set('view engine', 'html');
   app.engine('html', require('ejs').renderFile);  
+  //app.engine('html', require('hbs').__express);
   app.use(express.favicon());
   app.use(express.logger('dev'));
   app.use(express.bodyParser());
