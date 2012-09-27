3c4607bb536bf048b0024d8e57f43d013017e1a6
Jeff Capuano <jcapuano@commandalkon.com>
2012-09-27 15:29:47 -0400

    require'd as much as possible

--- web/public/javascripts/main.js
+++ web/public/javascripts/main.js
@@ -1,5 +1,38 @@
-require(["application", "utils/logger"], 
-function(Application, logger) {
+require.config({
+	paths: {
+    	//"socket.io": "http://localhost:4242/socket.io/socket.io.min",
+        //"jquery-ui.min": "http://ajax.aspnetcdn.com/ajax/jquery.ui/1.8.23/jquery-ui.min"//,
+        //"jquery.dataTables.min": "http://ajax.aspnetcdn.com/ajax/jquery.dataTables/1.9.1/jquery.dataTables.min"
+    },
+	shim: {
+    	'underscore': { attach: '_' },
+    	//'jquery-ui.min': ['jquery'],
+    	//'jquery.dataTables.min': ['jquery'],
+    	'jquery.validate.min': ['jquery'],
+    	'additional-methods.min': ['jquery.validate.min'],
+    	'jquery.json-2.3.min': ['jquery'],
+    	'jquery.history': {
+        	deps: ['jquery'],
+            attach: 'History'
+        },
+    	'amplify': { attach: 'amplify' },
+    	'handlebars-1.0.0.beta.6': { 	
+        	deps: ['jquery'],
+        	attach: 'Handlebars'
+		},
+    	'socket.io.min': { 	
+        	attach: 'io'
+		}
+    }
+});
+
+require(["lib/underscore", 
+		//"jquery-ui.min", "jquery.dataTables.min", 
+        "lib/jquery.validate.min", "lib/additional-methods.min", 
+        "lib/jquery.json-2.3.min", "lib/jquery.history", 
+		"lib/amplify", "lib/handlebars-1.0.0.beta.6", "lib/socket.io.min",
+        "application", "utils/logger"], 
+function(_, /*jqui, jqdt, */jqv, jqva, jqjson, jqhist, amp, hb, sio, Application, logger) {
     $(function() {
     	// required modules loaded
         logger.info("Running application");
